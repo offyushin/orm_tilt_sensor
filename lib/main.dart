1bae5f94303fbc266c195d8175ae8656ae3e72c2
Kim.Yushin <offyushin@naver.com>
2023-12-21 16:32:01 +0900

    tilt_sensor 센서표시모형 생성 및 sensors plus AccelerometerEvent 추가

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:sensors_plus/sensors_plus.dart';
 
 void main() {
   runApp(const MyApp());
@@ -25,23 +26,40 @@ class SensorApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    // 가로모드
+
     final centerX = MediaQuery.of(context).size.width / 2 - 50;
     final centerY = MediaQuery.of(context).size.height / 2 - 50;
 
     return Scaffold(
       body: Stack(
         children: [
-          Positioned(
-            left: centerX,
-            top: centerY,
-            child: Container(
-              decoration: const BoxDecoration(
-                color: Colors.green,
-                shape: BoxShape.circle,
-              ),
-              width: 100,
-              height: 100,
-            ),
+          StreamBuilder<AccelerometerEvent>(
+            stream: accelerometerEventStream(),
+            builder: (context, snapshot) {
+              if (!snapshot.hasData) {
+                return const Center(
+                  child: CircularProgressIndicator(),
+                );
+              }
+
+              final event = snapshot.data!;
+              List<double> accelerometerValues = [event.x, event.y, event.z];
+              print(accelerometerValues);
+
+              return Positioned(
+                left: centerX,
+                top: centerY,
+                child: Container(
+                  decoration: const BoxDecoration(
+                    color: Colors.green,
+                    shape: BoxShape.circle,
+                  ),
+                  width: 100,
+                  height: 100,
+                ),
+              );
+            }
           ),
         ],
       ),
